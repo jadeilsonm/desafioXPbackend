459bc31948b4e69044d6973c603496769cdd121c
Jadeilson Martinho <jadeilsonm17@gmail.com>
2022-07-16 18:31:10 -0300

    feat: model and migration of user

---
+++ src/model/User.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn({ type: 'integer'})
+    id: number;
+  @Column({ type: 'varchar', length: 100})
+    name: string;
+  @Column({ type: 'varchar', length: 150})
+    email: string;
+  @Column({ type: 'varchar', length: 255})
+    password: string;
+}
