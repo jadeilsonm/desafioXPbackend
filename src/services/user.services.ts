7afe4d7f769b575277c42636a2d5ec24e7d7f1da
Jadeilson Martinho <jadeilsonm17@gmail.com>
2022-07-20 17:07:44 -0300

    refactor: entity user

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -3,12 +3,12 @@ import IUser from '../interface/IUser';
 import UserRepository from '../repository/userRepository';
 import { generateJWTToken } from '../shared/JWT';
 
-
 const createdUser = async (user: IUser): Promise<IToken> => {
+  user.valor = 0;
   const newUser = UserRepository.create(user);
   await UserRepository.save(newUser);
   let token = '';  
-  if (newUser.id) {
+  if (newUser.codCliente) {
     token = generateJWTToken(JSON.stringify(user));
   }
   return { token };
