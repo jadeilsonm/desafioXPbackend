06f1b6dbd8de328dbd64308f34167b9544f4b000
Jadeilson Martinho <jadeilsonm17@gmail.com>
2022-07-22 22:52:32 -0300

    buld: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import { AppDataSource } from './data-source';
 import httpErrorMiddleware from './middleware/httpErrorMiddleware';
@@ -8,6 +9,8 @@ AppDataSource.initialize().then(() => {
   const app = express ();
 
   app.use(express.json());
+
+  app.use(cors());
 
   app.use(router);
 
